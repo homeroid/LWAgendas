434fe02d3a530a30dbd76f75400f837bb6bed2e4
homeroid <ome_gonza@hotmail.com>
2019-02-27 11:08:00 -0700

    Add project files.

---
+++ LWAgendaC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace LWAgendaC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ LWAgendaC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LWAgendaC.MvcApplication" Language="C#" %>
